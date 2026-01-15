f532bff2683815049c14ce4362c60e75138c5f46
Priyanshu <priyanshu12656@gmail.com>
2026-01-16 00:25:47 +0530

    chneg in css

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -10,7 +10,7 @@ export default function SiteLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow">{children}</main>
+      <main className="grow">{children}</main>
       <Footer />
     </div>
   );
